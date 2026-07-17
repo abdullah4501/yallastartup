5bdf06cb4648e56427409b7a7f61fd7a4e763643
Abdur Rahman <qaziabdurrahman12@gmail.com>
2026-07-17 18:44:22 +0500

    updatd

---
+++ dist/server/__vite_rsc_assets_manifest.js
@@ -0,0 +1,56 @@
+export default {
+  "bootstrapScriptContent": "import(\"/assets/index-B-qL4Kcg.js\")",
+  "clientReferenceDeps": {
+    "f2d0a8d0185e": {
+      "js": [
+        "/assets/scroll-animations-DH5pQe93.js",
+        "/assets/rolldown-runtime-S-ySWqyJ.js",
+        "/assets/framework-XU8gKkFC.js",
+        "/assets/index-B-qL4Kcg.js"
+      ],
+      "css": []
+    },
+    "339f946bc456": {
+      "js": [
+        "/assets/Icon-D4C-hQEA.js",
+        "/assets/rolldown-runtime-S-ySWqyJ.js",
+        "/assets/framework-XU8gKkFC.js",
+        "/assets/index-B-qL4Kcg.js"
+      ],
+      "css": []
+    },
+    "593f344dc510": {
+      "js": [
+        "/assets/index-B-qL4Kcg.js",
+        "/assets/rolldown-runtime-S-ySWqyJ.js",
+        "/assets/framework-XU8gKkFC.js"
+      ],
+      "css": []
+    },
+    "15c18cfaeeff": {
+      "js": [
+        "/assets/layout-segment-context-Du5kTC3a.js",
+        "/assets/rolldown-runtime-S-ySWqyJ.js",
+        "/assets/index-B-qL4Kcg.js",
+        "/assets/framework-XU8gKkFC.js"
+      ],
+      "css": []
+    },
+    "8c0f216c4604": {
+      "js": [
+        "/assets/index-B-qL4Kcg.js",
+        "/assets/rolldown-runtime-S-ySWqyJ.js",
+        "/assets/framework-XU8gKkFC.js"
+      ],
+      "css": []
+    }
+  },
+  "serverResources": {
+    "app/layout.tsx": {
+      "js": [],
+      "css": [
+        "/assets/index-IWSZq5zj.css"
+      ]
+    }
+  }
+}
